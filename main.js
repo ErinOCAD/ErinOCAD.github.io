373f85ca5b77e129c470ca944584184ee9b7c8ea
minniedino <3179143@student.ocadu.ca>
2021-02-26 10:38:21 -0500

    FriFeb26 10:38am

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ img.addEventListener("click", function()
 	{
 	
 	if(img.src != "waterforever.gif") {
-		img.setAttribute ("src", "waterforever.gif");
+		img.setAttribute("src", "waterforever.gif");
 	}
 	var stop = document.getElementById ("stop");
 	stop.addEventListener("click", function()
 	{
 		img.setAttribute("src", "plant.png");
 	})});
+
+var bunny = document.getElementById("bunny");
+bunny.addEventListener("click", function()
+	{
+	var speech = document.getElementById("speech");
+	speech.setAttribute("src", "heythere.png");
+	speech.setAttribute("width", "300px");
+	speech.setAttribute("height", "300px");
+	
+});
+
